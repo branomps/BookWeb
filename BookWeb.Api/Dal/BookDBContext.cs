2bf81302a1bd294e79242037aebd4779805191b2
BranislavMacosinec <branomps@gmail.com>
2017-10-14 12:37:38 +0100

    Add a DbContext clas in Dal.

---
+++ BookWeb.Api/Dal/BookDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BookWeb.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace BookWeb.Api.Dal
+{
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
