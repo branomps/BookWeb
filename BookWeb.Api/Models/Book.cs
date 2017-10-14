337d43ac966d9418659a421b394f61c7b14880bc
BranislavMacosinec <branomps@gmail.com>
2017-10-14 12:13:30 +0100

    Add Book Class to Models.

---
+++ BookWeb.Api/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookWeb.Api.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Release { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
