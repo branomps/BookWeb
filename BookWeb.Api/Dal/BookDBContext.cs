bf7d325e2837954c1939b081751ad620eadacf76
BranislavMacosinec <branomps@gmail.com>
2017-10-14 13:00:41 +0100

    add connection string name=BookConnectionString.

--- BookWeb.Api/Dal/BookDBContext.cs
+++ BookWeb.Api/Dal/BookDBContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,15 @@ namespace BookWeb.Api.Dal
 {
     public class BookDBContext : DbContext
     {
+        public BookDBContext(string BookConnectionString)
+        : base("name=BookConnectionString")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
         public DbSet<Book> Books { get; set; }
     }
 }
